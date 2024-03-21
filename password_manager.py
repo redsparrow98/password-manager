046fc3e90b8c4cdf9cfc0cd1d11f26faa2a2f80b
Natalija Prosic <prosicnatalija98@gmail.com>
2024-03-21 20:45:31 +0000

    PEP 8 update

--- password_manager.py
+++ password_manager.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
-import pyperclip
 import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -9,12 +8,18 @@ import json
 def password_generate():
     """generates a password for the user"""
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', \
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', \
+        'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', \
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',\
+        'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     # makes list for each element using list comprehension
-    # we generate a random range for each element and chose a random letter/symbol/num in the lists the amount of times in the range and assign to the new lists
+    # we generate a random range for each element and chose a
+    # random letter/symbol/num in the lists the amount of times
+    # in the range and assign to the new lists
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
@@ -31,20 +36,18 @@ def password_generate():
     # writes out the password to the box
     pass_entry.insert(0, password)
 
-    # computers at work struggle with copy paste option for pyperclip
-    # its meant to copy the password to be able to paste instantly as soon as its generated.
-    pyperclip.copy(password)
-
 # ---------------------------- PASSWORD FINDER ------------------------------- #
 
 def find_password():
+    """This function goes and finds the password for the website they have entered
+    if there is no password for that website lets the user know"""
     # get the data they entered in order to search the data
     website = web_entry.get()
 
     try:
         # try opening the file for the passwords and getting the data
         # in the form of a list
-        with open("data.json", "r") as data_file:
+        with open("data.json", "r", encoding= "uts-8") as data_file:
             data = json.load(data_file)
 
             # generate the email and password based on the website used to search
@@ -56,11 +59,13 @@ def find_password():
         messagebox.showerror(title="Error", message="No Data File Found")
 
     except KeyError:
-        # if the website they are trying to use does not exist in the data write error message
+        # if the website they are trying to use does not exist in the data
+        # write error message
         messagebox.showerror(title="Error", message=f"No details for {website} exist")
 
-    else: 
-        # if file exists and it contains the website info they are trying to use and we have email and password generated write out a message with the details
+    else:
+        # if file exists and it contains the website info they are trying to use and
+        # we have email and password generated write out a message with the details
         messagebox.showinfo(title=website, message=f"Email: {email} \nPassword: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -91,13 +96,13 @@ def save_pass():
     else:
         try:
             # try opening a file and updating data that we want to put back in to the json.
-            with open("data.json", "r") as data_file:
+            with open("data.json", "r", encoding= "uts-8") as data_file:
                 # read old data and save to variable
                 data = json.load(data_file)
 
         except FileNotFoundError:
             # in case file does not exist we create one and write the first entry(new_data)
-            with open("data.json", "w") as data_file:
+            with open("data.json", "w", encoding= "uts-8") as data_file:
                 json.dump(new_data, data_file, indent=4)
 
         else:
@@ -105,7 +110,7 @@ def save_pass():
             data.update(new_data)
 
             # then update the file with a new entry (data)
-            with open("data.json", "w") as data_file:
+            with open("data.json", "w", encoding= "uts-8") as data_file:
             # save updated data in the json file
                 json.dump(data, data_file, indent=4)
         finally:
@@ -129,9 +134,14 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 # labels
-web_label = Label(text="Website:").grid(column=0, row=1)
-em_usern_label = Label(text="Email/Username:").grid(column=0, row=2)
-pass_label = Label(text="Password:").grid(column=0,row=3)
+web_label = Label(text="Website:")
+web_label.grid(column=0, row=1)
+
+em_usern_label = Label(text="Email/Username:")
+em_usern_label.grid(column=0, row=2)
+
+pass_label = Label(text="Password:")
+pass_label.grid(column=0,row=3)
 
 # input spaces (entry)
 web_entry = Entry(width=24)
@@ -147,14 +157,14 @@ pass_entry.grid(column=1, row=3)
 
 
 # buttons
-generate_pass_butt = Button(text="Generate Password",highlightthickness=0, \
-    command=password_generate).grid(column=2,row=3)
+generate_pass_butt = Button(text="Generate Password",highlightthickness=0, command=password_generate)
+generate_pass_butt.grid(column=2,row=3)
 
-add_butt = Button(text="Add", width=39,highlightthickness=0, \
-    command=save_pass).grid(column=1, row=4, columnspan=2)
+add_butt = Button(text="Add", width=39,highlightthickness=0, command=save_pass)
+add_butt.grid(column=1, row=4, columnspan=2)
 
-Search_butt = Button(text="Search", width=14, highlightthickness=0, \
-    command=find_password).grid(column=2, row=1)
+Search_butt = Button(text="Search", width=14, highlightthickness=0, command=find_password)
+Search_butt.grid(column=2, row=1)
 
 # main loop to keep window on
 window.mainloop()
